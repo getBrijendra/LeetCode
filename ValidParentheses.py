903fcb25463758c0a0f62eeb6fe8df2e081640e8
getBrijendra <36080430+getBrijendra@users.noreply.github.com>
2021-09-08 18:59:11 +0530

    Update ValidParentheses.py

--- ValidParentheses.py
+++ ValidParentheses.py
@@ -1,1 +1,28 @@
 
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for c in s:
+            #print(stack)
+            if c == '(' or c == '{' or  c == '[':
+                stack.append(c)
+            elif c == ')':
+                if len(stack) != 0 and stack[-1] == '(':
+                    stack.pop(-1)
+                else:
+                    return False
+            elif c == '}':
+                if len(stack) != 0 and  stack[-1] == '{':
+                    stack.pop(-1)
+                else:
+                    return False
+            elif c == ']':
+                if len(stack) != 0 and  stack[-1] == '[':
+                    stack.pop(-1)
+                else:
+                    return False
+        
+        if len(stack) != 0:
+            return False
+        
+        return True
